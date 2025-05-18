380322285234e6ce9a26c9390f912eb9d973850b
Yehor_Yelenin <eelenin568@gmail.com>
2025-05-18 22:14:03 +0300

    MPI

--- QuickSortMPIOpenMP/Main.cpp
+++ QuickSortMPIOpenMP/Main.cpp
@@ -3,38 +3,88 @@
 #include <chrono>
 #include "QuickSort.h"
 #include "QuickSortOpenMP.h"
+#include <mpi.h>
 
-int main()
+//int main(int argc, char** argv)
+//{
+//	std::vector<int> array;
+//	int testSize = 600000;
+//
+//	for (int i = 0; i < testSize; ++i)
+//		array.push_back(testSize - i);
+//
+//	auto start = std::chrono::high_resolution_clock::now();
+//
+//	//QuickSort(array, 0, array.size() - 1);
+//	//QuickSortOpenMP(array, 8);
+//
+//	auto end = std::chrono::high_resolution_clock::now();
+//
+//	std::chrono::duration<double> duration = end - start;
+//	std::cout << "Час виконання: " << duration.count() << " секунд" << std::endl;
+//
+//	return 0;
+//}
+
+// MPI
+
+int main(int argc, char** argv)
 {
-	// Тестування
-	/*std::vector<int> test = {5, 3, 4, 1, 2, 3};
-	
-	QuickSort(test, 0, test.size() - 1);
+    MPI_Init(&argc, &argv);
 
-	for (int i : test)
-		std::cout << i;
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-	std::cout << std::endl;*/
+    const int testSize = 600000;
+    std::vector<int> fullArray;
 
-	std::vector<int> array;
-	int testSize = 600000;
+    // тільки rank 0 створює масив
+    if (rank == 0) {
+        fullArray.resize(testSize);
+        for (int i = 0; i < testSize; ++i)
+            fullArray[i] = testSize - i;
+    }
 
-	for (int i = 0; i < testSize; ++i)
-		array.push_back(testSize - i);
+    int chunkSize = testSize / size;
 
-	auto start = std::chrono::high_resolution_clock::now();
+    std::vector<int> localArray(chunkSize);
 
-	//QuickSort(array, 0, array.size() - 1);
-	QuickSortOpenMP(array, 8);
+    auto start = std::chrono::high_resolution_clock::now();
 
-	// MPI
+    // Розсилка по шматкам
+    MPI_Scatter(fullArray.data(), chunkSize, MPI_INT,
+        localArray.data(), chunkSize, MPI_INT,
+        0, MPI_COMM_WORLD);
 
+    // сортуємо локально
+    QuickSort(localArray, 0, chunkSize - 1);
 
+    // Збираємо результати
+    MPI_Gather(localArray.data(), chunkSize, MPI_INT,
+        fullArray.data(), chunkSize, MPI_INT,
+        0, MPI_COMM_WORLD);
 
-	auto end = std::chrono::high_resolution_clock::now();
+    if (rank == 0) 
+    {
+        // Зливаємо відсортовані шматки
+        std::vector<int> result = fullArray;
 
-	std::chrono::duration<double> duration = end - start;
-	std::cout << "Час виконання: " << duration.count() << " секунд" << std::endl;
+        int mergeSize = chunkSize;
+        while (mergeSize < testSize) {
+            for (int i = 0; i + mergeSize < testSize; i += 2 * mergeSize) {
+                std::inplace_merge(result.begin() + i,
+                    result.begin() + i + mergeSize,
+                    result.begin() + std::min(i + 2 * mergeSize, testSize));
+            }
+            mergeSize *= 2;
+        }
 
-	return 0;
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> duration = end - start;
+        std::cout << "Час виконання: " << duration.count() << " секунд" << std::endl;
+    }
+
+    MPI_Finalize();
+    return 0;
 }
--- QuickSortMPIOpenMP/QuickSortOpenMP.cpp
+++ QuickSortMPIOpenMP/QuickSortOpenMP.cpp
@@ -1,112 +1,6 @@
 #include "QuickSortOpenMP.h"
 #include "QuickSort.h"
 
-/*std::vector<int> QuickSortTasksOpenMP(std::vector<int> array)
-{
-	if (array.size() <= 1)
-		return array;
-
-	int pivot = array[array.size() - 1];
-	std::vector<int> left, right;
-	for (int i = 0; i < array.size() - 1; ++i)
-	{
-		if (array[i] < pivot)
-			left.push_back(array[i]);
-		else if (array[i] >= pivot)
-			right.push_back(array[i]);
-	}
-
-	std::vector<int> sortedLeft, sortedRight;
-
-	if (array.size() >= PARALLEL_THRESHOLD)
-	{
-		#pragma omp task shared(sortedLeft)
-			sortedLeft = QuickSortTasksOpenMP(left);
-		#pragma omp task shared(sortedRight)
-			sortedRight = QuickSortTasksOpenMP(right);
-	}
-	else
-	{
-		sortedLeft = QuickSort(left);
-		sortedRight = QuickSort(right);
-	}
-
-	sortedLeft.push_back(pivot);
-	sortedLeft.insert(sortedLeft.end(), sortedRight.begin(), sortedRight.end());
-	return sortedLeft;
-}
-
-std::vector<int> QuickSortOpenMP(std::vector<int> array)
-{
-	std::vector<int> result;
-
-	#pragma omp parallel
-	{
-		#pragma omp single
-			result = QuickSortTasksOpenMP(array);
-		#pragma omp taskwait
-	}
-
-	return result;
-}
-
-//std::vector<int> QuickSortOpenMP(std::vector<int> array)
-//{
-//	if (array.size() <= 1)
-//		return array;
-//
-//	int pivot = array[array.size() - 1];
-//	std::vector<int> left, right;
-//	for (int i = 0; i < array.size() - 1; ++i)
-//	{
-//		if (array[i] < pivot)
-//			left.push_back(array[i]);
-//		else if (array[i] >= pivot)
-//			right.push_back(array[i]);
-//	}
-//
-//	std::vector<int> sortedLeft, sortedRight;
-//
-//	if (array.size() > PARALLEL_THRESHOLD)
-//	{
-//		// Створює пул потоків
-//		#pragma omp parallel
-//		{
-//			// Тільки один потік з пула має виконати цей блок, тобто тільки один потік створить задачі 
-//			// (nowait щоб інші потоки не чекали на завершення цього single блоку)
-//			#pragma omp single nowait
-//			{
-//				// Створює задачу, яку може виконати будь-який потік з пулу 
-//				// shared(sortedLeft) вказує, що змінна sortedLeft спільна для всіх потоків 
-//				// та може бути змінена в середині задачі
-//				#pragma omp task shared(sortedLeft)
-//				{
-//					sortedLeft = QuickSortOpenMP(left);
-//				}
-//
-//				// Теж саме
-//				#pragma omp task shared(sortedRight)
-//				{
-//					sortedRight = QuickSortOpenMP(right);
-//				}
-//
-//				// Чекати, поки всі створені задачі в цій області завершаться
-//				// Потрібно, бо при злитті масиву всі частини мають бути відсортовані
-//				#pragma omp taskwait
-//			}
-//		}
-//	}
-//	else
-//	{
-//		sortedLeft = QuickSortOpenMP(left);
-//		sortedRight = QuickSortOpenMP(right);
-//	}
-//
-//	sortedLeft.push_back(pivot);
-//	sortedLeft.insert(sortedLeft.end(), sortedRight.begin(), sortedRight.end());
-//	return sortedLeft;
-//}*/
-
 std::vector<int> QuickSortOpenMP(std::vector<int>& array, int numThreads)
 {
 	int n = array.size();
@@ -130,25 +24,28 @@ std::vector<int> QuickSortOpenMP(std::vector<int>& array, int numThreads)
 	}
 
 	// Попарне злиття відсортованих частин
-	while (chunks.size() > 1)
+	while (chunks.size() > 1) // Зливаємо, поки більше одного шматку
 	{
-		std::vector<std::vector<int>> newChunks;
+		std::vector<std::vector<int>> newChunks; // кладемо сюди злиті шматки
 
-		#pragma omp parallel for schedule(dynamic) num_threads(numThreads)
-		for (int i = 0; i < chunks.size() / 2; ++i)
+		#pragma omp parallel for schedule(dynamic) num_threads(numThreads) // schedule(dynamic) - omp може перекидати ітерації поміж потоків
+		for (int i = 0; i < chunks.size() / 2; ++i) // запускаємо цикл для кожної пари шматків
 		{
 			std::vector<int> merged;
-			std::merge(chunks[2 * i].begin(), chunks[2 * i].end(),
+			std::merge(chunks[2 * i].begin(), chunks[2 * i].end(), // з'єднуємо два відсортовані шматки у один відсортований. std::merge гарантує порядок
 				chunks[2 * i + 1].begin(), chunks[2 * i + 1].end(),
 				std::back_inserter(merged));
 
-			#pragma omp critical
+			// щоб не було перегонів, коли декілька потоків одночасно додають у newChunks, використовуємо цю директиву, щоб в кожен момент часу тільки один потік
+			// міг робити push_back
+			#pragma omp critical 
 			newChunks.push_back(std::move(merged));
 		}
 
-		if (chunks.size() % 2 == 1)
+		if (chunks.size() % 2 == 1) // випадок, коли кількість шматків непарна. Просто переносимо останній у newChunks
 			newChunks.push_back(std::move(chunks.back()));
 
+		// тепер newChunks стає chunks та нова ітерація
 		chunks = std::move(newChunks);
 	}
 
